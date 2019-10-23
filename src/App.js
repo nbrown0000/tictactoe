fd03dc99825eedb935e7a59f6eb73936a5955fd9
nbrown0000 <brown.nathanjames@gmail.com>
2019-10-23 22:28:51 +1100

    built grid and added response to clicking each cell

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      grid: ['','','','','','','','',''],
+      player1: {}, // uses crosses (X)
+      computer: {} // uses noughts (O)
+    }
+  }
+
+  cellClicked(event) {
+    console.log(event)
+    this.setState({grid: this.state.grid.map((item,i) => {
+      if(event===i) item='X'
+      return item
+    })})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className='header'>
+          <h1>Tic Tac Toe</h1>
+        </div>
+        
+        <div className='grid'>
+          {this.state.grid.map((item,i) => {
+            return (
+              <div
+                key={i}
+                className='cell'
+                onClick={() => this.cellClicked(i)}
+              >
+                {item}
+              </div>
+            )
+          })}
+        </div>
+
+        <div className='footer'>
+          <p>Designed and built by Nathan Brown</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
