929aa81f77c4f334f62b339d465a53139c199928
nbrown0000 <brown.nathanjames@gmail.com>
2019-10-25 17:12:24 +1100

    Added checking for current cell values

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React from 'react';
 import './App.css';
 
-class App extends React.Component {
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+class Game extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      gameOver: false,
       grid: ['','','','','','','','',''],
-      player1: {}, // uses crosses (X)
-      computer: {} // uses noughts (O)
+      turn: 'player'
     }
   }
 
+  componentDidUpdate() {
+    if(this.state.turn === 'computer') this.computersMove();
+  }
+
   cellClicked(event) {
-    console.log(event)
     this.setState({grid: this.state.grid.map((item,i) => {
-      if(event===i) item='X'
-      return item
+      if(event===i) {
+        if(this.state.grid[i]==='') { item='X'; this.setState({turn: 'computer'})}
+        if(this.state.grid[i]==='X') { item='X'; this.setState({turn: 'player'})}
+        if(this.state.grid[i]==='O') { item='O'; this.setState({turn: 'player'})}
+      }
+      return item;
     })})
   }
 
+  possibleMoves() {
+    var moves = [];
+    this.state.grid.forEach((item,i) => {if(item==='') moves.push(i)})
+    return moves;
+  }
+
+  //computers turn algorithm
+  computersMove = () => {
+    sleep(500).then(()=>{
+      // play random move
+      const moves = this.possibleMoves();
+      const randomMoveLocation = moves[Math.floor(Math.random()*moves.length)]
+      this.setState({grid: this.state.grid.map((item,i) => {
+        if(randomMoveLocation===i) item='O';
+        return item;
+      })})
+    })
+
+    console.log('computer played a move');
+    this.setState({turn: 'player'})
+  }
+
   render() {
+
     return (
       <div className="App">
         <div className='header'>
@@ -42,10 +76,12 @@ class App extends React.Component {
 
         <div className='footer'>
           <p>Designed and built by Nathan Brown</p>
+          <p>{this.state.turn}'s turn...</p>
+          <p>{`Is game over? ${this.state.gameOver}`}</p>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default Game;
