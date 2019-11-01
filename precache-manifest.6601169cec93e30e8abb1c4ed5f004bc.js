6e369e648e07dbf5653e8aa6b3c70c1c8788fdf1
nbrown0000 <brown.nathanjames@gmail.com>
2019-11-01 22:27:51 +1100

    Updates

---
+++ precache-manifest.6601169cec93e30e8abb1c4ed5f004bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4857c48ec47e72219bfdff3be7c67043",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "95be17fce181308b431c",
+    "url": "/tictactoe/static/css/main.3e7ff6f2.chunk.css"
+  },
+  {
+    "revision": "2940945365f56b026105",
+    "url": "/tictactoe/static/js/2.f3a6b57b.chunk.js"
+  },
+  {
+    "revision": "95be17fce181308b431c",
+    "url": "/tictactoe/static/js/main.ac664c65.chunk.js"
+  },
+  {
+    "revision": "1dd1878a39f419eb6036",
+    "url": "/tictactoe/static/js/runtime-main.868a150c.js"
+  }
+]);
