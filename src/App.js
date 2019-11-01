9e886f3f2f121135d7bdac406a580fb728c55f87
nbrown0000 <brown.nathanjames@gmail.com>
2019-11-01 22:23:20 +1100

    finished computer player logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import './App.css';
 
-function sleep (time) {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
-
 class Game extends React.Component {
   constructor(props) {
     super(props)
@@ -15,6 +11,10 @@ class Game extends React.Component {
       computerSymbol: 'O',
       turn: 'player'
     }
+  }
+  
+  sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
   }
 
   componentDidUpdate() {
@@ -34,13 +34,13 @@ class Game extends React.Component {
 
   possibleMoves() {
     var moves = [];
-    this.state.grid.forEach((item,i) => {if(item==='') moves.push(i+1)})
+    this.state.grid.forEach((item,i) => {if(item==='') moves.push(i)})
     return moves;
   }
 
   cornerMovesAvailable() {
     // if cell 1,3,7,9 available return true
-    if(this.possibleMoves().some(r => [1,3,7,9].includes(r))) return true
+    if(this.possibleMoves().some(r => [0,2,6,8].includes(r))) return true
     // else return false
     else return false
   }
@@ -53,60 +53,192 @@ class Game extends React.Component {
     return result;
   }
 
-  positionsTaken(symbol) {
+  positionsTakenBy(symbol) {
     var result = [];
     this.state.grid.forEach((item,i) => {
-      if(item===symbol) result.push(i+1)
+      if(item===symbol) result.push(i)
     })
     return result;
   }
 
+
+
   columnToDefend() {
-    if(this.intersection(this.positionsTaken(this.state.playerSymbol),[1,4,7]).length===2) return 1
-    if(this.intersection(this.positionsTaken(this.state.playerSymbol),[2,5,8]).length===2) return 2
-    if(this.intersection(this.positionsTaken(this.state.playerSymbol),[3,6,9]).length===2) return 3
+    const { playerSymbol, computerSymbol } = this.state;
+    if(this.intersection(this.positionsTakenBy(playerSymbol),[0,3,6]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[0,3,6]).length===0) {
+        return 1
+      }
+    }
+    else if(this.intersection(this.positionsTakenBy(playerSymbol),[1,4,7]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[1,4,7]).length===0) {
+        return 2
+      }
+    }
+    else if(this.intersection(this.positionsTakenBy(playerSymbol),[2,5,8]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[2,5,8]).length===0) {
+        return 3
+      }
+    }
+    else return 0
+  }
+
+  rowToDefend() {
+    const { playerSymbol, computerSymbol } = this.state;
+    if(this.intersection(this.positionsTakenBy(playerSymbol),[0,1,2]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[0,1,2]).length===0) {
+        return 1
+      }
+    }
+    if(this.intersection(this.positionsTakenBy(playerSymbol),[3,4,5]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[3,4,5]).length===0) {
+        return 2
+      }
+    }
+    if(this.intersection(this.positionsTakenBy(playerSymbol),[6,7,8]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[6,7,8]).length===0) {
+        return 3
+      }
+    }
+  }
+
+  leftDiagToDefend() {
+    const { playerSymbol, computerSymbol } = this.state;
+    if(this.intersection(this.positionsTakenBy(playerSymbol),[0,4,8]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[0,4,8]).length===0) {
+        return true
+      }
+    }
+    else return false
+  }
+
+  rightDiagToDefend() {
+    const { playerSymbol, computerSymbol } = this.state;
+    if(this.intersection(this.positionsTakenBy(playerSymbol),[2,4,6]).length===2) {
+      if(this.intersection(this.positionsTakenBy(computerSymbol),[2,4,6]).length===0) {
+        return true
+      }
+    }
+    else return false
+  }
+
+  playMove(position, symbol) {
+    this.setState({grid: this.state.grid.map((item,i) => {
+      if(i===position) item=symbol;
+      return item
+    })})
+
+    const { grid, playerSymbol, computerSymbol } = this.state;
+    if(grid[0]===playerSymbol && grid[4]===playerSymbol && grid[8]===playerSymbol) {
+      this.setState({gameOver: true})
+    }
+
+    
   }
 
   //computers turn algorithm
   computersMove = () => {
-    const { playerSymbol, computerSymbol, grid } = this.state
+    const { computerSymbol, grid } = this.state
 
-    sleep(300).then(()=>{
-      // // play random move
-      // const moves = this.possibleMoves();
-      // const randomMoveLocation = moves[Math.floor(Math.random()*moves.length)]
-      // this.setState({grid: this.state.grid.map((item,i) => {
-      //   if(randomMoveLocation===i) item='O';
-      //   return item;
-      // })})
+    // sleep(300).then(()=>{
 
+      // Computers First Move
       if(this.possibleMoves().length===8) {
-        const availGoodMoves = this.intersection(this.possibleMoves(),[1,3,7,9]);
+        const availGoodMoves = this.intersection(this.possibleMoves(),[0,2,6,8]);
         const chosenGoodMove = availGoodMoves[Math.floor(Math.random()*availGoodMoves.length)]
-        this.setState({grid: grid.map((item,i) => {
-          if(chosenGoodMove===i+1) item=computerSymbol;
-          return item;
-        })})
+        this.playMove(chosenGoodMove,computerSymbol)
+        this.setState({turn: 'player'})
       }
 
-      switch(this.columnToDefend()) {
-        case 1:
-          console.log('column 1');
-          // play move with spot
-          break;
-        case 2:
-          console.log('column 2');
-          // play move with spot
-          break;
-        case 3:
-          console.log('column 3');
-          // play move with spot
-          break;
-        default: break;
-
+      // defend columns
+      else if(this.columnToDefend()) {
+        console.log('Defend a column')
+        switch(this.columnToDefend()) {
+          case 1:
+            // defend column 1
+            if(grid[0]==='') this.playMove(0,computerSymbol)
+            else if(grid[3]==='') this.playMove(3,computerSymbol)
+            else if(grid[6]==='') this.playMove(6,computerSymbol)
+            this.setState({turn: 'player'})
+            break;
+          case 2:
+            // defend column 2
+            if(grid[1]==='') this.playMove(1,computerSymbol)
+            else if(grid[4]==='') this.playMove(4,computerSymbol)
+            else if(grid[7]==='') this.playMove(7,computerSymbol)
+            this.setState({turn: 'player'})
+            break;
+          case 3:
+            // defend column 3
+            if(grid[2]==='') this.playMove(2,computerSymbol)
+            else if(grid[5]==='') this.playMove(5,computerSymbol)
+            else if(grid[8]==='') this.playMove(8,computerSymbol)
+            this.setState({turn: 'player'})
+            break;
+          default: break;
+        }
       }
 
-    })
+      // defend rows
+      else if(this.rowToDefend()) {
+        switch(this.rowToDefend()) {
+          case 1:
+            // defend row 1
+            if(grid[0]==='') this.playMove(0,computerSymbol)
+            else if(grid[1]==='') this.playMove(1,computerSymbol)
+            else if(grid[2]==='') this.playMove(2,computerSymbol)
+            this.setState({turn: 'player'})
+            break;
+          case 2:
+            // defend row 2
+            if(grid[3]==='') this.playMove(3,computerSymbol)
+            else if(grid[4]==='') this.playMove(4,computerSymbol)
+            else if(grid[5]==='') this.playMove(5,computerSymbol)
+            this.setState({turn: 'player'})
+            break;
+          case 3:
+            // defend row 3
+            if(grid[6]==='') this.playMove(6,computerSymbol)
+            else if(grid[7]==='') this.playMove(7,computerSymbol)
+            else if(grid[8]==='') this.playMove(8,computerSymbol)
+            this.setState({turn: 'player'})
+            break;
+          default: break;
+        }
+      }
+
+      else if(this.leftDiagToDefend()) {
+        console.log("Defend left diagonal!")
+        if(grid[0]==='') this.playMove(0,computerSymbol)
+        else if(grid[4]==='') this.playMove(4,computerSymbol)
+        else if(grid[8]==='') this.playMove(8,computerSymbol)
+        this.setState({turn: 'player'})
+      }
+
+      else if(this.rightDiagToDefend()) {
+        console.log("Defend right diagonal!")
+        if(grid[2]==='') this.playMove(2,computerSymbol)
+        else if(grid[4]==='') this.playMove(4,computerSymbol)
+        else if(grid[6]==='') this.playMove(6,computerSymbol)
+        this.setState({turn: 'player'})
+      }
+      
+      // random move
+      else {
+        const moves = this.possibleMoves()
+        const chosenMove = moves[Math.floor(Math.random()*moves.length)]
+        this.playMove(chosenMove, computerSymbol)
+        this.setState({turn: 'player'})
+      }
+
+      
+    // }) // end sleep
+
+    //console.log(this.positionsTaken(this.state.playerSymbol));
+    // this.sleep(300).then(() => {
+    //   console.log(this.positionsTaken(this.state.playerSymbol).concat(this.positionsTaken(computerSymbol)))
+    // })
+    
 
     console.log('computer played a move');
     this.setState({turn: 'player'})
@@ -137,7 +269,7 @@ class Game extends React.Component {
         <div className='footer'>
           <p>Designed and built by Nathan Brown</p>
           <p>{this.state.turn}'s turn...</p>
-          <p>{`Is game over? ${this.state.gameOver}`}</p>
+          {this.state.gameOver ? <p>GAME OVER!</p>: <p></p> }
         </div>
       </div>
     );
